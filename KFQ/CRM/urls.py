8880cc05d61849d4797707b03cfa7c266d885f2b
19tak <wjdxkrdl123@naver.com>
2021-06-29 09:51:05 +0900

    Fix Hospital - Sidebar

---
+++ KFQ/CRM/urls.py
@@ -0,0 +1,30 @@
+from django.urls import path
+
+from CRM import views
+
+app_name='CRM'
+urlpatterns = [
+    # path('test/', views.index),
+
+# ---------------------------------- [edit] ---------------------------------- #
+    #path('theme/practice/lst_hospital', views.theme.theme_hospital, name='hospital'),
+    path('theme/practice/', views.theme.theme_practice, name='practice'),
+    path('theme/practice/<int:question_id>/', views.theme.theme_practice_detail, name='practice_detail'),
+    path('theme/practice/answer/create/<int:question_id>/', views.theme.theme_practice_ans_create, name='practice_ans_create'),
+#***********************************************************************#
+    path('theme/hospital', views.theme.theme_hospital, name='hospital'),
+#***********************************************************************#
+    # Original Theme
+    path('theme/index/', views.theme.theme_index, name='01_index.html'),
+    path('theme/profile/', views.theme.theme_profile, name='02_profile.html'),
+    path('theme/settings/', views.theme.theme_settings, name='03_settings.html'),
+    path('theme/invoice/', views.theme.theme_invoice, name='04_invoice.html'),
+    path('theme/blank/', views.theme.theme_blank, name='05_blank.html'),
+    path('theme/UI_Element/page=<page>', views.theme.theme_UI_Element, name='06_UI_Element.html'),
+    path('theme/icons/', views.theme.theme_icons, name='07_icons.html'),
+    path('theme/forms/page=<page>', views.theme.theme_Forms, name='08_Forms.html'),
+    path('theme/tables', views.theme.theme_tables, name='09_tables.html'),
+    path('theme/charts', views.theme.theme_chart, name='10_charts.html'),
+    path('theme/maps', views.theme.theme_maps, name='11_maps.html'),
+    # path('theme//', views.index, name=''),
+]
---
+++ KFQ/CRM/views.py
@@ -0,0 +1,157 @@
+from django.core import paginator
+from django.http import HttpResponse
+from django.shortcuts import redirect, render, get_object_or_404
+from .models import Question, Hospital
+from django.utils import timezone
+from django.core.paginator import Paginator
+
+# Create your views here.
+def index(request):
+    return HttpResponse("CRM PAGE.")
+    
+#***********************************************************************#
+# Original Theme
+class theme :
+    def theme_index(request):
+        print("PAGE : theme_index")
+        page ='Dashboard'
+        context = {
+            'page' : page
+        }
+        return render(request, './theme/01_index.html', context)
+
+    def theme_profile(request):
+        print("PAGE : theme_profile")
+        page ='Profile'
+        context = {
+            'page' : page
+        }
+        return render(request, './theme/02_profile.html', context)
+
+    def theme_settings(request):
+        print("PAGE : theme_settings")
+        page ='Settings'
+        context = {
+            'page' : page
+        }
+        return render(request, './theme/03_settings.html', context)
+
+    def theme_invoice(request):
+        print("PAGE : theme_invoice")
+        page ='Invoice'
+        context = {
+            'page' : page
+        }
+        return render(request, './theme/04_invoice.html', context)
+
+    def theme_blank(request):
+        print("PAGE : theme_blank")
+        page ='Blank'
+        context = {
+            'page' : page
+        }
+        return render(request, './theme/05_blank.html', context)
+# ---------------------------------- [edit] ---------------------------------- #
+    def theme_hospital(request):
+        print("PAGE : theme_hospital")
+        page = 'Hospital'
+        pg = request.GET.get('page','1')
+        lst_hospital = Hospital.objects.all()
+        paginator = Paginator(lst_hospital, 15) # 페이지 정리, 15개씩
+        pg_obj = paginator.get_page(pg)
+        context = {'lst_hospital':pg_obj, 'page':page}
+        return render(request, './theme/lst_hospital.html', context)
+
+    def theme_practice(request):
+        print("PAGE : theme_practice")
+        page ='Practice'
+        pg = request.GET.get('page','1') # 입력파라미터, 페이지
+        question_list = Question.objects.order_by('-create_date') # 조회
+        paginator = Paginator(question_list, 10) # 페이지 정리, 10개씩
+        pg_obj = paginator.get_page(pg)
+        context = {'question_list': pg_obj,
+        'page' : page}
+        return render(request, './theme/05_practice.html', context)
+        
+    def theme_practice_detail(request, question_id):
+        print("PAGE : theme_practice_detail")
+        page ='Practice'
+        question = get_object_or_404(Question, pk=question_id)
+        context = {'question': question,
+        'page' : page}
+        return render(request, './theme/05_practice_detail.html', context)
+
+    def theme_practice_ans_create(request, question_id):
+        print("PAGE : theme_practice_ans_create")
+        page ='Practice'
+        question = get_object_or_404(Question, pk=question_id)
+        question.answer_set.create(
+            content=request.POST.get('content'), create_date=timezone.now())
+        return redirect('CRM:practice_detail', question_id=question.id)
+# ---------------------------------- [edit] ---------------------------------- #
+    def theme_UI_Element(request, page):
+        print("PAGE : ",page)
+
+        context = {
+            'page' : page
+        }
+        if page == "01-alerts.html":
+            return render(request, './theme/UI_Element/ui-alerts.html', context)
+        elif page == "02-buttons.html":
+            return render(request, './theme/UI_Element/ui-buttons.html', context)
+        elif page == "03-cards.html":
+            return render(request, './theme/UI_Element/ui-cards.html', context)
+        elif page == "04-general.html":
+            return render(request, './theme/UI_Element/ui-general.html', context)
+        elif page == "05-grid.html":
+            return render(request, './theme/UI_Element/ui-grid.html', context)
+        elif page == "06-modals.html":
+            return render(request, './theme/UI_Element/ui-modals.html', context)
+        elif page == "07-typography.html":
+            return render(request, './theme/UI_Element/ui-typography.html', context)
+
+        return render(request, './theme/06_UI_Element.html', context)
+
+    def theme_icons(request):
+        print("PAGE : theme_icons")
+        page ='Icons'
+        context = {
+            'page' : page
+        }
+        return render(request, './theme/07_icons.html', context)
+
+    def theme_Forms(request, page):
+        print("PAGE : theme_Forms",page)
+
+        context = {
+            'page' : page
+        }
+        if page == "01-layouts.html":
+            return render(request, './theme/Forms/'+page, context)
+        elif page == "02-basic-inputs.html":
+            return render(request, './theme/Forms/'+page, context)
+
+    def theme_tables(request):
+        print("PAGE : theme_tables")
+        page ='Tables'
+        context = {
+            'page' : page
+        }
+        return render(request, './theme/09_tables.html', context)
+
+    def theme_chart(request):
+        print("PAGE : theme_Charts")
+        page ='Charts'
+        context = {
+            'page' : page
+        }
+        return render(request, './theme/10_charts.html', context)
+
+    def theme_maps(request):
+        print("PAGE : theme_maps")
+        page ='Maps'
+        context = {
+            'page' : page
+        }
+        return render(request, './theme/11_maps.html', context)
+#***********************************************************************#
